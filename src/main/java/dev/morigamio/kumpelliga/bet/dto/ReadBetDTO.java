4342fdc1a8ac92884e7f1ea9b448385c890069f9
Moritz <moritz_rose@posteo.de>
2026-09-02 15:15:14 +0200

    implemented doubleBets in the backend, no frontend yet

--- src/main/java/dev/morigamio/kumpelliga/bet/dto/ReadBetDTO.java
+++ src/main/java/dev/morigamio/kumpelliga/bet/dto/ReadBetDTO.java
@@ -2,13 +2,14 @@ package dev.morigamio.kumpelliga.bet.dto;
 
 import dev.morigamio.kumpelliga.bet.Bet;
 
-public record ReadBetDTO(Long id, Long gameId, String prediction, float stake) {
+public record ReadBetDTO(Long id, Long gameId, String prediction, float stake, boolean isDouble) {
     public static ReadBetDTO from(Bet bet){
         return new ReadBetDTO(
                 bet.getId(),
                 bet.getGame() != null ? bet.getGame().getId() : null,
                 bet.getPrediction(),
-                bet.getStake()
+                bet.getStake(),
+                bet.isDouble()
         );
     }
 }
