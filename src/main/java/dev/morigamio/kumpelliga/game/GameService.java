f18a503d91f3ea8fc11b2ef5d8362c61acb3b915
Moritz <moritz_rose@posteo.de>
2026-09-04 19:26:32 +0200

    implemented full method to sync games and store the latest update time stamp per gameday in the new gameday table

--- src/main/java/dev/morigamio/kumpelliga/game/GameService.java
+++ src/main/java/dev/morigamio/kumpelliga/game/GameService.java
@@ -5,6 +5,7 @@ import dev.morigamio.kumpelliga.game.spi.GameDataClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -37,6 +38,8 @@ public class GameService {
         try {
             List<GameData> gameData = gameDataClient.retrieveGameData();
             List<Game> games = new ArrayList<>();
+            LocalDateTime updateTimeStamp = LocalDateTime.MIN;
+            int previousGameDay = 0;
             for (GameData data : gameData) {
                 Game game = gamePersistence.findById(data.gameId()).orElse(new Game());
                 game.setId(data.gameId());
@@ -49,12 +52,70 @@ public class GameService {
                 game.setWinner(determineWinner(data.goalsHomeTeam(), data.goalsAwayTeam()));
                 game.setFinished(data.matchFinished());
                 games.add(game);
+
+                // if next game day is reached, store latest update timestamp and reset the variable for next gameday
+                int currentGameDay = data.gameDay();
+                if (previousGameDay != 0 && currentGameDay != previousGameDay) {
+                    gamePersistence.storeGameDay(new GameDay(previousGameDay, updateTimeStamp));
+                    updateTimeStamp = LocalDateTime.MIN;
+                }
+                previousGameDay = currentGameDay;
+
+                // update latest update timestamp
+                boolean isTimeStampNewer = data.updateTimeStamp().isAfter(updateTimeStamp);
+                if (isTimeStampNewer) updateTimeStamp = data.updateTimeStamp();
+            }
+            // store latest update timestamp for last gameday
+            if (previousGameDay != 0) {
+                gamePersistence.storeGameDay(new GameDay(previousGameDay, updateTimeStamp));
             }
             gamePersistence.storeGames(games);
             log.info("Games synchronized successfully.");
         } catch (Exception e) {
             log.error("syncGames: ", e);
         }
+    }
+
+    public void syncGamesByGameDay(int gameDay) {
+        try {
+            List<GameData> gameData = gameDataClient.retrieveGameData(gameDay);
+            LocalDateTime updateTimeStamp = LocalDateTime.MIN;
+            List<Game> games = new ArrayList<>();
+            for (GameData data : gameData) {
+                Game game = gamePersistence.findById(data.gameId()).orElse(new Game());
+                game.setId(data.gameId());
+                game.setHomeTeam(data.homeTeam());
+                game.setAwayTeam(data.awayTeam());
+                game.setGameDay(data.gameDay());
+                game.setMatchTime(data.matchTime());
+                game.setGoalsHomeTeam(data.goalsHomeTeam());
+                game.setGoalsAwayTeam(data.goalsAwayTeam());
+                game.setWinner(determineWinner(data.goalsHomeTeam(), data.goalsAwayTeam()));
+                game.setFinished(data.matchFinished());
+                games.add(game);
+
+                // update latest retrieval timestamp
+                boolean isTimeStampNewer = data.updateTimeStamp().isAfter(updateTimeStamp);
+                if (isTimeStampNewer) updateTimeStamp = data.updateTimeStamp();
+            }
+            gamePersistence.storeGames(games);
+            gamePersistence.storeGameDay(new GameDay(gameDay, updateTimeStamp));
+            log.info("Games for gameday %s synchronized successfully. Set UpdateTimestamp to %s".formatted(gameDay, updateTimeStamp));
+        } catch (Exception e) {
+            log.error("syncGamesByGameDay: ", e);
+        }
+    }
+
+    public boolean isGameDataUpToDate(int gameDay) {
+        LocalDateTime oldTimeStamp = gamePersistence.findGameDay(gameDay)
+                .orElseThrow(() -> new RuntimeException("There is no timestamp for gameday %s to retrieve." + gameDay))
+                .getUpdateTimeStamp();
+        LocalDateTime newTimeStamp = gameDataClient.retrieveUpdateTimeStamp(gameDay);
+        return oldTimeStamp.equals(newTimeStamp);
+    }
+
+    public List<GameDay> getAllGameDays(){
+        return gamePersistence.findAllGameDays();
     }
 
     private String determineWinner(int goalsHomeTeam, int goalsAwayTeam) {
