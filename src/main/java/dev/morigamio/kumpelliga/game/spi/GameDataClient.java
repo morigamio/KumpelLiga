aa54cab6d1c363e07258d52d34bd77c616d3ce99
Moritz <moritz_rose@posteo.de>
2026-09-04 19:16:51 +0200

    added two methods to retrieve gamedata per gameday and updatetimestamps per gameday for optimized livetracking

--- src/main/java/dev/morigamio/kumpelliga/game/spi/GameDataClient.java
+++ src/main/java/dev/morigamio/kumpelliga/game/spi/GameDataClient.java
@@ -1,7 +1,10 @@
 package dev.morigamio.kumpelliga.game.spi;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface GameDataClient {
     List<GameData> retrieveGameData();
+    List<GameData> retrieveGameData(int gameDay);
+    LocalDateTime retrieveUpdateTimeStamp(int gameDay);
 }
